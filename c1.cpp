c232945f0d006d93930f3223ae3f385fe50bca13
dark-Repo <atharva9421@gmail.com>
2026-09-17 23:23:02 +0530

    Move Zero to End

--- c1.cpp
+++ c1.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main()
+class college
 {
-
-    return 0;
+    string name;
+    int age;
+};
+class student : public college
+{
+    int marks;
+    student(string name, int age, int marks)
+    {
+        cout << name << age << marks << endl;
+    }
+} int main()
+{
+    student s1("arahan", 19, 99)
 }
